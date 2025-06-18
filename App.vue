afae41ffefaf73d0b45f66d552899dad15f51c93
Albert <albert.ubanje@rra.gov.rw>
2025-06-18 20:27:51 +0200

    kiddohub website init code

--- App.vue
+++ App.vue
@@ -2,10 +2,38 @@
   <div id="app">
     <header>
       <!-- Header Content -->
-      Huong Hua Co. Ltd.
+      Kiddo Hub Initiative.
     </header>
 
     <main>
+      <!-- Image Lightbox -->
+      <MDBContainer class="mt-5">
+        <MDBLightbox>
+          <MDBRow>
+            <MDBCol lg="6">
+              <MDBLightboxItem
+                src="https://mdbootstrap.com/img/Photos/Thumbnails/Slides/1.webp"
+                fullscreenSrc="https://mdbootstrap.com/img/Photos/Slides/1.webp"
+                class="w-100 mb-2 mb-md-4"
+              />
+              <MDBLightboxItem
+                src="https://mdbootstrap.com/img/Photos/Thumbnails/Square/1.webp"
+                fullscreenSrc="https://mdbootstrap.com/img/Photos/Square/1.webp"
+                class="w-100"
+              />
+            </MDBCol>
+            <MDBCol lg="6">
+              <MDBLightboxItem
+                src="https://mdbootstrap.com/img/Photos/Thumbnails/Vertical/1.webp"
+                fullscreenSrc="https://mdbootstrap.com/img/Photos/Vertical/1.webp"
+                class="w-100"
+              />
+            </MDBCol>
+          </MDBRow>
+        </MDBLightbox>
+      </MDBContainer>
+
+      <!-- Existing router view -->
       <router-view></router-view>
     </main>
 
@@ -15,19 +43,40 @@
   </div>
 </template>
 
+
 <script>
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBLightbox,
+  MDBLightboxItem
+} from "mdb-vue-ui-kit";
+
+// If you're using other components like SendButton, import them too
+import SendButton from "./components/SendButton.vue";
+import ChatbotContainer from "./components/ChatbotContainer.vue";
+import ChatbotHeader from "./components/ChatbotHeader.vue";
+import ChatbotBody from "./components/ChatbotBody.vue";
+import ChatbotInput from "./components/ChatbotInput.vue";
+
 export default {
   name: "App",
   components: {
-    // Import components here
-    send_button: SendButton,
-    chatbot_container: ChatbotContainer,
-    chatbot_header: ChatbotHeader,
-    chatbot_body: ChatbotBody,
-    chatbot_input: ChatbotInput,
+    SendButton,
+    ChatbotContainer,
+    ChatbotHeader,
+    ChatbotBody,
+    ChatbotInput,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBLightbox,
+    MDBLightboxItem,
   },
 };
 </script>
+
 
 <style>
 #app {
